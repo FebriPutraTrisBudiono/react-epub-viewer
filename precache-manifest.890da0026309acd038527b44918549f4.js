155a610a3c9f296178a15f60f6d150ac9132f631
FebriPutraTrisBudiono <febript@gmail.com>
2022-10-07 09:18:40 +0700

    Updates

---
+++ precache-manifest.890da0026309acd038527b44918549f4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "1aba7c900199b25468ab",
+    "url": "/static/js/main.1aba7c90.chunk.js"
+  },
+  {
+    "revision": "b89dfadfa336b1927ab8",
+    "url": "/static/js/1.b89dfadf.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9b99337e6b85173bcec5bdab46856ab6",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/epub/precache-manifest.35398ee35761aeba3ddce40378a901a0.js"
+  "/precache-manifest.890da0026309acd038527b44918549f4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/epub/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
